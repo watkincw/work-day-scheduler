6c15df2212bb9b7635edc11b2a9b4008d34b8813
Colton <watkincw@gmail.com>
2021-06-28 15:32:45 -0500

    added current date and time to the header of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
+// DISPLAY DATE AND TIME LOGIC //
+
+const dateTime = document.getElementById("currentDate")
+
+function updateDateTime() { 
+	// get current date and time
+	const now = moment().format("[Today is] dddd, MMMM Do YYYY, hh:mm:ss A");
+	
+	// place 'now' into <p> with id="currentDate" with the js variable dateTime
+	dateTime.innerText = now
+};
+setInterval(updateDateTime, 1000)
+
+// Display Date and Time END //
+// SAVE BUTTONS LOGIC
+
 const saveBtn = document.querySelectorAll('button')
 
-
-console.log(saveBtn);
-
-// for loop to give each button localStorage functionality to the save buttons 
+// for loop to give localStorage functionality to saveBtn's 
 for (var i = 0; i < saveBtn.length; i++) { 
 
+	// when a saveBtn is clicked,
 	saveBtn[i].addEventListener("click", function() { 
 		// save data in localStorage
-		alert(i)
+		localStorage.setItem('textarea')
 	})
-	
+
 }
+
