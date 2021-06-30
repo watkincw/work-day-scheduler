0a099f66a2e4fbfa879f3859c9b75ff6db8bfa5d
Colton <watkincw@gmail.com>
2021-06-30 12:18:52 -0500

    added js logic to save hourly tasks and load them on open/reload/refresh of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,48 +16,44 @@ setInterval(updateDateTime, 1000)
 
 const saveBtn = document.querySelectorAll('button') // targets ALL button elements -- used in SAVE BUTTONS
 const textAreaText = document.querySelectorAll('textarea') // targets ALL textarea elements -- used in LOAD TASKS
-let taskText = document.getElementById("task").value // selecting the value/input/text of elements with 'task' as an ID -- USED IN SAVE BUTTONS AND LOAD TASKS 
 
 
 // GLOBAL VARABLES FOR DAY PLANNER SECTION END //
 // SAVE BUTTONS LOGIC
-
-
 // for loop to save taskText to localStorage on saveBtn click -- updates/saves ALL fields at once
 for (let i = 0; i < saveBtn.length; i++) { 
 	// when a saveBtn is clicked,
 	saveBtn[i].addEventListener("click", function() { 
-
 		
+		let taskId = 'task' + i
+		// save taskText
+		let taskText = document.getElementById(taskId).value
 		// save data in localStorage
-		localStorage.setItem('taskText', taskText)
+		localStorage.setItem('taskText' + i, taskText)
+		
 	})
 	
 }
 
 // SAVE BUTTONS LOGIC END //
-
-// ------------------------------------------------------------------------------------
-// 1. Save tasks to localStorage in more than just 9am slot
-// 2. Make tasks in time slots persist on load/refresh
-// ------------------------------------------------------------------------------------
-
 // LOAD TASKS LOGIC //
 
 
-// for loop to input tasks in correct time slots on page load/refresh - from localStorage
-for (let i = 0; i < textAreaText.length; i++) { // on page load, not on button click
-	
-	// When page is reloaded/refreshed,
-	textAreaText[i].addEventListener("load", function() {
+const loadTasks = function() { 
+	// for loop to input tasks in correct time slots on page load/refresh - from localStorage
+	for (let i = 0; i < textAreaText.length; i++) { // on page load, not on button click
+
 		// getItem from localStorage
-		localStorage.getItem('taskText', taskText)
-		
+		let taskText = localStorage.getItem('taskText' + i)
+
 		// input it into the proper time field
-		document.getElementById("task").value = taskText;
-	})
-	
+		document.getElementById("task" + i).value = taskText;
+
+	}
+
 }
 
+
 // LOAD TASKS LOGIC END //
+loadTasks();
 console.log(localStorage);
